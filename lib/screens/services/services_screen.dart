6e86549f6802344b6d288d1e6274f71976bc5686
Lucas Emanuel <luc4s.emanuel@gmail.com>
2021-09-27 11:47:00 -0300

    Added resume volunteers months

--- lib/screens/services/services_screen.dart
+++ lib/screens/services/services_screen.dart
@@ -1,5 +1,9 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:louvor_app/common/custom_drawer/custom_drawer.dart';
+import 'package:louvor_app/helpers/app_list_pool.dart';
+import 'package:louvor_app/models/Service.dart';
 import 'package:louvor_app/models/service_manager.dart';
 import 'package:louvor_app/screens/services/components/service_list_tile.dart';
 import 'package:provider/provider.dart';
@@ -15,6 +19,9 @@ class ServicesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    List<Service> lstServicesUsedAsResume = [];
+
     return Scaffold(
       drawer: CustomDrawer(),
       appBar: AppBar(
@@ -83,6 +90,8 @@ class ServicesScreen extends StatelessWidget {
       body: Consumer<ServiceManager>(
         builder: (_, serviceManager, __) {
           final filteredServices = serviceManager.filteredServicesByMounth(filterByMounth);
+          lstServicesUsedAsResume = [];
+          filteredServices.forEach((service) => lstServicesUsedAsResume.add(service));
           return ListView.builder(
               padding: const EdgeInsets.all(4),
               itemCount: filteredServices.length,
@@ -91,6 +100,88 @@ class ServicesScreen extends StatelessWidget {
               });
         },
       ),
+        floatingActionButton:
+        Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            FloatingActionButton(
+              child: Icon(
+                Icons.list,
+                size: 30,
+              ),
+              onPressed: () {
+                showAlertDialog1(context, lstServicesUsedAsResume);
+              },
+            ),
+          ],
+        )
+
     );
   }
+
+  showAlertDialog1(BuildContext context, List<Service> lstServicesUsedAsResume) {
+    // configura o button
+    Widget okButton = FlatButton(
+      child: Text("OK"),
+      onPressed: () {  Navigator.of(context).pop(); },
+    );
+
+    // configura o  AlertDialog
+    AlertDialog alerta = AlertDialog(
+      title: Text("Resumo alocações mês"),
+      content: Text(volunteersMonthResume(lstServicesUsedAsResume)),
+      actions: [
+        okButton,
+      ],
+    );
+    // exibe o dialog
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return alerta;
+      },
+    );
+  }
+
+  String volunteersMonthResume(List<Service> lstServicesOfMonth){
+    String stringResume = '';
+
+    Map<String, Map<String, int>> mapRoleVolunteerQuantity = new Map();
+
+    lstServicesOfMonth.forEach((service) {
+                                            if(service.team != null){
+                                                service.team.forEach((role, listVolunteer) {
+                                                      addToMap(mapRoleVolunteerQuantity, role, listVolunteer);
+                                                });
+                                            }
+                                         }
+                               );
+
+    mapRoleVolunteerQuantity.forEach((role, volunteers) {
+                                                          stringResume += role+'\n';
+                                                          volunteers.forEach((volunteer, quantity) {
+                                                            stringResume += '\t\t- ${volunteer} : ${quantity}\n';
+                                                          });
+
+                                    });
+  return stringResume+'\n';
+
+  }
+  
+  void addToMap(Map<String, Map<String, int>> mapRoleVolunteerQuantity, String role, List<String> listVolunteer){
+
+    listVolunteer.forEach((volunteer) {
+      if(mapRoleVolunteerQuantity.containsKey(role)){
+        if(mapRoleVolunteerQuantity[role].containsKey(volunteer)){
+          mapRoleVolunteerQuantity[role][volunteer] = mapRoleVolunteerQuantity[role][volunteer]+1;
+        }else{
+          mapRoleVolunteerQuantity[role].putIfAbsent(volunteer, () => 1);
+        }
+      }else{
+        mapRoleVolunteerQuantity.putIfAbsent(role, () => {volunteer: 1});
+      }
+    });
+
+  }
+
 }
