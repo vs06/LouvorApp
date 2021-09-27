a00529e3c2ace4b32a9775893bbc76442e96f813
Lucas Emanuel <luc4s.emanuel@gmail.com>
2021-09-27 15:42:32 -0300

    Blocked users screen for not admin users

--- lib/common/custom_drawer/custom_drawer.dart
+++ lib/common/custom_drawer/custom_drawer.dart
@@ -45,23 +45,15 @@ class CustomDrawer extends StatelessWidget {
                 title: 'Cultos',
                 page: 2,
               ),
-//              Visibility(
-//              visible: UserManager.isUserAdmin,
-//                child:
-                DrawerTile(
-                  iconData: Icons.supervised_user_circle_sharp,
-                  title: 'Usuários',
-                  page: 3,
-                ),
-//              ),
-              // Visibility(
-              //   visible: UserManager.isUserAdmin,
-              //   child: DrawerTile(
-              //     iconData: Icons.build,
-              //     title: 'Usuários',
-              //     page: 4,
-              //   ),
-              // ),
+               Visibility(
+                 visible: UserManager.isUserAdmin != null ? UserManager.isUserAdmin : false,
+                 child:
+                     DrawerTile(
+                       iconData: Icons.supervised_user_circle_sharp,
+                       title: 'Usuários',
+                       page: 3,
+                     ),
+               )
             ],
           ),
         ],
